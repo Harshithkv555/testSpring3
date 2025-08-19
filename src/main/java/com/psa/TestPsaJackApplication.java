ced1955e10dad603fd901960120ecc5926044326
harsha <harshakv79@gmail.com>
2025-08-19 11:41:23 +0530

    first commit

---
+++ src/main/java/com/psa/TestPsaJackApplication.java
@@ -0,0 +1,14 @@
+package com.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPsaJackApplication {
+
+	public static void main(String[] args) {
+		int x =10;
+		SpringApplication.run(TestPsaJackApplication.class, args);
+	}
+
+}
